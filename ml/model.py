7ddcda146fb3e1c8571a0d63967406455893f55d
LollaYasasviAnirudh <anirudh.yasasvi@techoptima.ai>
2026-04-02 16:02:35 +0530

    Replaced large dataset with smaller dataset

--- ml/model.py
+++ ml/model.py
@@ -1,4 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv("data/resumes.csv")
-print(df.head())
+# Load full dataset
+df = pd.read_csv("data/resumes_small.csv")
+
+# Take only first 500 rows (small dataset)
+df_small = df.head(1000)
+
+# Save smaller dataset
+df_small.to_csv("data/resumes_small.csv", index=False)
+
+print("Small dataset created successfully!")
+print(df_small.shape)
